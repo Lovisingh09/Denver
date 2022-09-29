1b3dbbcef70df66f136afcec08d88495f16f0f5e
Js62264 <58326525+Js62264@users.noreply.github.com>
2022-09-29 17:18:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,7 +243,7 @@ async def start(client, message):
             f_caption=f_caption
     if f_caption is None:
             f_caption = f"{files.file_name}"
-        k = await client.send_cached_media(
+            k = await client.send_cached_media(
             chat_id=message.from_user.id,
             file_id=file_id,
             caption=f_caption,
