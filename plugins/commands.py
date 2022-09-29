84dac534c3fb5f24ef79ec709a5bf93ebdf9cdb2
Js62264 <58326525+Js62264@users.noreply.github.com>
2022-09-29 16:40:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -87,6 +87,7 @@ async def start(client, message):
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
         ]]
+            
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
